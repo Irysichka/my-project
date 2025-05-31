509eef61a7be4dae60ef6b5a74d279e93251f83e
Irysichka <Irysichka2704@gmail.com>
2025-05-31 13:40:15 +0000

    Deploying to gh-pages from @ Irysichka/my-project@d48940f5f62f305af23c655748e0501b8613fb2c 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,7 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as e}from"./assets/vendor-CgTBfC_f.js";const a=document.querySelector(".gallery");function r(l){return`
+    <li class="gallery-item">
+      <a class="gallery-link" href="${original}">
+        <img class="gallery-image" src="${preview}" alt="${description}" />
+      </a>
+    </li>)`.join("")}a.insertAdjacentHTML("beforeend",r());new e(".gallery a",{captionsData:"alt",captionDelay:250});
+//# sourceMappingURL=1-gallery.js.map

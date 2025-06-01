4ac23282494591473317d5ae7e3ea35e93b017fa
Irina <Irysichka2704@gmail.com>
2025-06-01 17:14:10 +0300

    add new inform

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,10 +1,25 @@
-const formData = {
+let formData = {
     email: "",
     message: ""
 };
 
+const form = document.querySelector(".feedback-form");
 const KEY = "feedback-form-state";
 
+form.addEventListener("input", event => {
+    const {name, value} = event.target;
+    if (name === "email" || name === "message")
+        formData[name] = value;
+    localStorage.setItem(KEY, JSON.stringify(formData))
+});
 
 
-
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (!formData.email || !formData.message) {
+      alert("Fill please all fields");
+        return;}
+    console.log(formData);
+    form.reset();
+    localStorage.removeItem(KEY);
+});
